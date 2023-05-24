4517cf73ec48884bbdeecd5162da023b5d1f00a3
HARSH SHUKLA <ashuklaharsh2005@gmail.com>
2023-05-25 00:38:44 +0530

    Design modifications.

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 // import noodles from "./noodles.png"
 import twoLeaves from "./2_leaves.png"
+// import leaves from "./leaves.png"
 import Landing from './Landing'
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
             <circle cx="700" cy="150" r="200" fill="black" />
       </svg> */}
       {/* <img src={noodles} className="fixed w-1/6 -rotate-45 -translate-x-[15vw] translate-y-[29vh]" /> */}
-      <img src={twoLeaves} className="absolute translate-x-80 translate-y-[25vh] rotate-75 w-1/4 h-1/3 z-30" id="bowl" />
+      <img src={twoLeaves} className="absolute translate-x-80 translate-y-[28vh] rotate-75 w-1/4 h-1/2 z-30" id="bowl" />
+      {/* <img src={leaves} className="absolute -translate-x-[10vw] translate-y-[28vh] rotate-75 w-1/4 h-1/2 z-20" id="bowl" /> */}
     
 
     </div>
--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -2,9 +2,9 @@ import "@fortawesome/fontawesome-free/css/all.css";
 
 function Header() {
    return (
-      <div className="w-full pl-20 px-8 py-4 flex flex-row justify-between items-center z-40 ">
+      <div className="w-full pl-20 pr-12 px-8 py-8 flex flex-row justify-between items-center z-40 ">
          <i className="fa fa-burger text-black text-2xl"></i>
-         <ul className="flex flex-row gap-[2vw] font-semibold text-[3vh]">
+         <ul className="flex flex-row gap-[2vw] font-semibold text-[2vh]">
             <li>Menu</li>
             <li>Lunch Boxes</li>
             <li>Platters</li>
